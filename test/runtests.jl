bda3e266cf6622ed25667bec875a80d94d51cdd6
Min Thet Khine <mtkhine@mit.edu>
2022-09-27 09:32:13 -0400

    Add initial test for tnsread

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,13 @@ using Test
 
 @testset "TensorMarket.jl" begin
     # Write your tests here.
+    matmul_result = (
+        (
+            [1, 3, 1, 3, 2, 4, 2, 4], 
+            [1, 2, 3, 4, 1, 2, 3, 4], 
+            [1, 1, 2, 2, 3, 3, 4, 4]
+        ), 
+        [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+    )
+    @test tnsread("matmul_2-2-2.tns") == matmul_result
 end
